3d7b30fe88408441f5a4f2a848ef394a2b99891e
MyGit01 <MyGit01@163.com>
2019-08-08 11:17:40 +0800

    commit first

---
+++ src/com/atguigu/demo/Demo1.java
@@ -0,0 +1,17 @@
+package com.atguigu.demo;
+
+import java.util.Scanner;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		/*System.out.println("HelloWorld");
+		int[] arr = { 11, 22, 33 };
+		for (int i : arr) {
+			System.out.println(i);
+		}*/
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+	}
+
+}
